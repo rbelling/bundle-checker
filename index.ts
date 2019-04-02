a407f0942de6dde2231e5cc43476f77fa52c2ede
Riccardo Bellingeri <riccardo.bellingeri@hbc.com>
2019-04-02 14:18:04 +0100

    tidy: rename parameters, refactor async code

--- index.ts
+++ index.ts
@@ -11,66 +11,62 @@ import {
 } from "./types/bundle-checker-types";
 // const { log, warn, error } = console;
 
-const defaultParams: IBundleCheckerParams = {
+const dummyParams: IBundleCheckerParams = {
   buildScript: "cd ./example; npm run build",
   distPath: path.resolve(__dirname, "./example/dist"),
   sizeLimit: 1.5 * 1024 * 1024,
-  targetFiles: ["**/*.css", "**/*.js"]
+  targetFilesPattern: ["**/*.css", "**/*.js"]
 };
 const exec = util.promisify(childProcessExec);
 const spinner = ora();
 
 /**
- * Run the build script, then return the files matched by the targetFiles glob
+ * Run the build script, then return the files matched by the targetFilesPattern glob
  * @param buildScript
- * @param targetFiles
+ * @param targetFilesPattern
  */
 const getBuiltFiles = async (
   buildScript: string,
-  targetFiles: string[]
+  targetFilesPattern: string[]
 ): Promise<string[]> => {
   spinner.start(`Running build script: \`${buildScript}\``);
   await exec(buildScript);
   spinner.succeed();
-  return globby(targetFiles);
+
+  return globby(targetFilesPattern);
 };
 
 const generateStats = async ({
   buildScript,
   distPath = "",
   sizeLimit,
-  targetFiles
-}: IBundleCheckerParams): Promise<IBundleCheckerReport> =>
-  new Promise(async (resolve, reject) => {
-    try {
-      const builtFiles = await getBuiltFiles(
-        buildScript,
-        targetFiles.map(item => path.resolve(distPath, item))
-      );
-      const size = await getSize(builtFiles);
-      const sizeSurplus = size.parsed - sizeLimit;
-      const prettyBundleSize = getPrettierFileSize(size.parsed);
-      const prettyBundleLimit = getPrettierFileSize(sizeLimit);
-      if (sizeSurplus > 0) {
-        reject(
-          `ERROR: Project is currently ${prettyBundleSize}, which is ${getPrettierFileSize(
-            sizeSurplus
-          )} larger than the maximum allowed size (${prettyBundleLimit}).`
-        );
-      }
-      resolve({
-        reportText: `SUCCESS: Total bundle size of ${prettyBundleSize} is less than the maximum allowed size (${prettyBundleLimit})`
-      });
-    } catch (err) {
-      reject(err);
-    }
-  });
+  targetFilesPattern
+}: IBundleCheckerParams): Promise<IBundleCheckerReport> => {
+  const builtFiles = await getBuiltFiles(
+    buildScript,
+    targetFilesPattern.map(item => path.resolve(distPath, item))
+  );
+  const size = await getSize(builtFiles);
+  const sizeSurplus = size.parsed - sizeLimit;
+  const prettyBundleSize = getPrettierFileSize(size.parsed);
+  const prettyBundleLimit = getPrettierFileSize(sizeLimit);
+  if (sizeSurplus > 0) {
+    throw new Error(
+      `ERROR: Project is currently ${prettyBundleSize}, which is ${getPrettierFileSize(
+        sizeSurplus
+      )} larger than the maximum allowed size (${prettyBundleLimit}).`
+    );
+  }
+  return {
+    reportText: `SUCCESS: Total bundle size of ${prettyBundleSize} is less than the maximum allowed size (${prettyBundleLimit})`
+  };
+};
 
 (async () => {
   spinner.start(`Checking bundle size`);
-  // Todo: this will come from CLI params, instead of using defaultParams
+  // Todo: this will come from CLI params, instead of using dummyParams
   try {
-    const { reportText } = await generateStats(defaultParams);
+    const { reportText } = await generateStats(dummyParams);
     spinner.succeed(reportText);
     process.exit(0);
   } catch (e) {
--- types/bundle-checker-types.d.ts
+++ types/bundle-checker-types.d.ts
@@ -3,12 +3,12 @@ export interface IBundleCheckerReport {
 }
 
 export interface IBundleCheckerParams {
-  // Optional distPath represents the dist folder. If present, all of the targetFiles globs will be prepended with this
+  // Optional distPath represents the dist folder. If present, all of the targetFilesPattern globs will be prepended with this
   distPath?: string;
   // The build script that will be run as part of the bundle checker.
   buildScript: string;
-  // Target number of Bytes of parsed code (non-gzipped)
+  // Byte-size limit of parsed code (non-gzipped)
   sizeLimit: number;
-  // An array of files that we want to compare with another branch. This can contain globs too.
-  targetFiles: string[];
+  // An array of patterns that is used to target distribution files.
+  targetFilesPattern: string[];
 }
