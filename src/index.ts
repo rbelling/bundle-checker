fe73e410e221a0d437584912c85982e3d164cbbc
Riccardo Bellingeri <riccardo.bellingeri@hbc.com>
2019-04-03 18:33:14 +0100

    feat(getBundleSize): extracted build/install steps into a separate function for reuse
    fixed ora spinner to correctly mark start/end of a task

--- src/index.ts
+++ src/index.ts
@@ -11,46 +11,45 @@ import {
 } from "../types/bundle-checker-types";
 
 const exec = util.promisify(childProcessExec);
-const spinner = ora();
 
 const installDependencies = async (
   bundleCheckerParams: IBundleCheckerParams
 ): Promise<undefined> => {
   const { installScript } = bundleCheckerParams;
-  spinner.start(`Installing dependencies with: \`${installScript}\``);
+  const spinner = ora(`Installing dependencies with: \`${installScript}\``);
+  spinner.start();
   await exec(installScript);
   spinner.succeed();
 
   return;
 };
 
-/**
- * Run the build script, then return the files matched by the targetFilesPattern glob
- * @param bundleCheckerParams
- */
-const getBuiltFiles = async (
+const getBundleSize = async (
   bundleCheckerParams: IBundleCheckerParams
-): Promise<string[]> => {
-  const { buildScript, distPath, targetFilesPattern } = bundleCheckerParams;
+): Promise<number> => {
+  await installDependencies(bundleCheckerParams);
+  const getTargetedFiles = async (): Promise<string[]> => {
+    const { buildScript, distPath, targetFilesPattern } = bundleCheckerParams;
+    const spinner = ora(`Running build script: \`${buildScript}\``);
 
-  spinner.start(`Running build script: \`${buildScript}\``);
-  await exec(buildScript);
-  spinner.succeed();
+    spinner.start();
+    await exec(buildScript);
+    spinner.succeed();
 
-  return globby(targetFilesPattern.map(item =>
-    path.resolve(distPath, item)
-  ) as ReadonlyArray<string>);
+    return globby(targetFilesPattern.map(item =>
+      path.resolve(distPath, item)
+    ) as ReadonlyArray<string>);
+  };
+  return (await getSize(await getTargetedFiles())).parsed;
 };
 
 export const generateBundleStats = async (
   bundleCheckerParams: IBundleCheckerParams
 ): Promise<IBundleCheckerReport> => {
-  await installDependencies(bundleCheckerParams);
-  const builtFiles = await getBuiltFiles(bundleCheckerParams);
-  const size = await getSize(builtFiles);
-  const prettyBundleSize = prettyPrint(size.parsed);
+  const bundleSize = await getBundleSize(bundleCheckerParams);
+  const prettyBundleSize = prettyPrint(bundleSize);
   const prettyBundleLimit = prettyPrint(bundleCheckerParams.sizeLimit);
-  const sizeSurplus = size.parsed - bundleCheckerParams.sizeLimit;
+  const sizeSurplus = bundleSize - bundleCheckerParams.sizeLimit;
   const reportText =
     sizeSurplus > 0
       ? `WARN: Project is currently ${prettyBundleSize}, which is ${prettyPrint(
