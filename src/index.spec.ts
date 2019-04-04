1b773bb5f86accf84a469c482e06b3df0d148e2d
Riccardo Bellingeri <rbelling@users.noreply.github.com>
2019-04-04 12:31:49 +0100

    Compare `n` branches (#5)
    * feat(generateBundleStats): pass through bundleChecker object instead of destructuring individual entries
    
    made distPath non-optional as it simplifies the logic. this will have to be passed in as an empty string
    
    * feat: install dependencies before comparing bundles
    
    * feat(getBundleSize): extracted build/install steps into a separate function for reuse
    
    fixed ora spinner to correctly mark start/end of a task
    
    * feat: check bundle size on different branches
    
    * tidy: reuse more code
    
    * feat: memoize bundle checker functions (revealing module)
    
    * feat(compareBranches): generalize to make it work with n branches (not just 2)

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,6 @@
 import * as path from "path";
 import { IBundleCheckerParams } from "../types/bundle-checker-types";
-import { generateBundleStats } from "./index";
+import bundleChecker from "./index";
 
 const TEN_MINUTES = 10 * 60 * 1000;
 const ONE_MEGABYTE = 1 * 1024 * 1024;
@@ -9,6 +9,7 @@ const HUNDRED_KILOBYTES = ONE_MEGABYTE / 10;
 export const dummyParams: IBundleCheckerParams = {
   buildScript: `cd ${path.resolve(__dirname, "../example")}; yarn build`,
   distPath: path.resolve(__dirname, "../example/dist"),
+  installScript: `cd ${path.resolve(__dirname, "../example")}; yarn`,
   sizeLimit: ONE_MEGABYTE,
   targetFilesPattern: ["**/*.css", "**/*.js"]
 };
@@ -18,7 +19,9 @@ describe("Bundle Stats tests", () => {
 
   test("Bundle report text contains the word `SUCCESS` if sizeLimit is set to 1MB", async done => {
     try {
-      const { reportText } = await generateBundleStats(dummyParams);
+      const { reportText } = await bundleChecker(
+        dummyParams
+      ).generateBundleStats();
       expect(reportText).toContain("SUCCESS");
       done();
     } catch (e) {
@@ -27,14 +30,25 @@ describe("Bundle Stats tests", () => {
   });
   test("Bundle report text contains the word `WARN` if sizeLimit is set to 100kB", async done => {
     try {
-      const { reportText } = await generateBundleStats({
+      const { reportText } = await bundleChecker({
         ...dummyParams,
         sizeLimit: HUNDRED_KILOBYTES
-      });
+      }).generateBundleStats();
       expect(reportText).toContain("WARN");
       done();
     } catch (e) {
       done.fail(e);
     }
+  });
+
+  test(`bundle size of two branches`, async done => {
+    const { reportText } = await bundleChecker(dummyParams).compareBranches(
+      "master",
+      "feat/git-workflow"
+    );
+
+    console.log(reportText);
+    expect(true).toBe(true);
+    done();
   });
 });
