24856abceaa85d26ca676b275c42aa43e21aabee
Riccardo Bellingeri <riccardo.bellingeri@hbc.com>
2019-04-04 14:55:07 +0100

    fix(git commands): only stash if dirty

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,25 +1,25 @@
-import * as path from "path";
-import { IBundleCheckerParams } from "../types/bundle-checker-types";
-import bundleChecker from "./index";
+import * as path from 'path';
+import { IBundleCheckerParams } from '../types/bundle-checker-types';
+import bundleChecker from './index';
 
 const TEN_MINUTES = 10 * 60 * 1000;
 const ONE_MEGABYTE = 1 * 1024 * 1024;
 
 export const dummyParams: IBundleCheckerParams = {
-  buildScript: `cd ${path.resolve(__dirname, "../example")}; yarn build`,
-  distPath: path.resolve(__dirname, "../example/dist"),
-  installScript: `cd ${path.resolve(__dirname, "../example")}; yarn`,
+  buildScript: `cd ${path.resolve(__dirname, '../example')}; yarn build`,
+  distPath: path.resolve(__dirname, '../example/dist'),
+  installScript: `cd ${path.resolve(__dirname, '../example')}; yarn`,
   sizeLimit: ONE_MEGABYTE,
-  targetFilesPattern: ["**/*.js"]
+  targetFilesPattern: ['**/*.js']
 };
 
-describe("Bundle Stats tests", () => {
+describe('Bundle Stats tests', () => {
   jest.setTimeout(TEN_MINUTES);
 
   test(`bundle size of two branches`, async done => {
     const { reportText } = await bundleChecker(dummyParams).compareBranches(
-      "feat/add-useless-dependencies-to-example",
-      "master"
+      'feat/add-useless-dependencies-to-example',
+      'master'
     );
 
     console.log(reportText);
