29d1394b7e112a1bbef7c8a746f91f17403fee10
Riccardo Bellingeri <riccardo.bellingeri@hbc.com>
2019-04-04 14:26:24 +0100

    feat: add useless code to test that bundle size checker works

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,8 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
+import babelConfig from '../babel.config';
+const { log } = console;
+
+const identity = (anything: any) => {
+  log(JSON.stringify(babelConfig, anything);
+  return anything;
+};
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount(identity('#app'));
